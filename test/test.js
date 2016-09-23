9d741bc4931335eee213d160850985e7b66610ac
Alessandro Zanardi <alessandro@zanardi.org>
2016-09-23 04:39:04 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var should = require('chai').should();
+
+var clonator = require('../index.js');
+var mocks = require('./mocks.js');
+
+describe('clonator', function () {
+
+  it('should clone primitive values', function () {
+    clonator(mocks.num).should.equal(mocks.num);
+    clonator(mocks.string).should.equal(mocks.string);
+    clonator(mocks.bool).should.equal(mocks.bool);
+    clonator(mocks.func).should.equal(mocks.func);
+    clonator('').should.equal('');
+    should.equal(clonator(null), null);
+    should.equal(clonator(undefined), undefined);
+  });
+
+  it('should clone composite values', function () {
+    clonator(mocks.arr).should.eql(mocks.arr);
+    clonator(mocks.obj).should.eql(mocks.obj);
+    clonator(mocks.nestedArr).should.eql(mocks.nestedArr);
+    clonator(mocks.nestedObj).should.eql(mocks.nestedObj);
+  });
+
+});
