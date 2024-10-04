89fc5f84911ba1b714d4161e53186a3f8621d534
CW Self Paced Student <testselfpaced@gmail.com>
2024-10-04 16:07:31 +0000

    done

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 // it gets passed. The generated copy should have the same own
 // enumerable properties as the source, but modifying
 // the copy should not affect the source in any way.
-// Just a little comment
 
 function clonator (source) {
    if (Array.isArray(source)) {
